4faa62f2dd13495385a8931d2bf7e02c38d1e0a4
Sarvesh kumar singh <87657797+Sarveshhttp420notfound@users.noreply.github.com>
2024-10-07 11:52:33 +0530

    Add files via upload

---
+++ Question_26_Pattern_14.java
@@ -0,0 +1,17 @@
+package level_2_Pattern;
+
+import java.util.Scanner;
+
+public class Question_26_Pattern_14 {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		int x=scn.nextInt();
+		for(int i=1;i<=10;i++) {
+			int v=x*i;
+			System.out.println(x +"*"+i+"="+v);
+		}
+
+	}
+
+}
