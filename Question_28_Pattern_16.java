7deda4056ad68034784e10cbc74d1a8620114e56
Sarvesh kumar singh <87657797+Sarveshhttp420notfound@users.noreply.github.com>
2024-10-08 14:00:36 +0530

    Add files via upload

---
+++ Question_28_Pattern_16.java
@@ -0,0 +1,39 @@
+package level_2_Pattern;
+
+import java.util.Scanner;
+
+public class Question_28_Pattern_16 {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int star=1;
+		int space=2*n-3;
+		for(int i=1;i<=n;i++) {
+			int val=1;
+			for(int j=1;j<=star;j++) {
+				System.out.print(val+"\t");
+				val++;
+			}
+			for(int j=1;j<=space;j++) {
+				System.out.print("\t");
+			}
+			if(i==n) {
+				star--;
+				val--;
+			}
+			for(int j=1;j<=star;j++) {
+				val--;
+				System.out.print(val+"\t");
+			}
+			
+			System.out.println();
+			
+			star++;
+			space -=2;
+//			System.out.println();
+		}
+
+	}
+
+}
